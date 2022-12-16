b2b227fecafb9cd31614475314688d2acd032079
Thinh24 <119273108+Thinh24@users.noreply.github.com>
2022-12-16 10:50:27 +0700

    cập nhật giao diện và dữ liệu giả

---
+++ resources/views/web/footer.blade.php
@@ -0,0 +1,40 @@
+<div class="container py-4">
+    <div class="row">
+        <div class="col-md-3 col-xs-6">
+            <div class="gioithieu">
+                <h3 class="header text-uppercase font-weight-bold">Về DealBook</h3>
+                <a href="#">Giới thiệu về DealBook</a>
+                <a href="#">Tuyển dụng</a>
+                <div class="fb-like" data-href="https://www.facebook.com/DealBook-110745443947730/"
+                     data-width="300px" data-layout="button" data-action="like" data-size="small"
+                     data-share="true"></div>
+            </div>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <div class="hotrokh">
+                <h3 class="header text-uppercase font-weight-bold">HỖ TRỢ KHÁCH HÀNG</h3>
+                <a href="#">Hướng dẫn đặt hàng</a>
+                <a href="#">Phương thức thanh toán</a>
+                <a href="#">Phương thức vận chuyển</a>
+                <a href="#">Chính sách đổi trả</a>
+            </div>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <div class="lienket">
+                <h3 class="header text-uppercase font-weight-bold">HỢP TÁC VÀ LIÊN KẾT</h3>
+                <img src="tem/images/dang-ky-bo-cong-thuong.png" alt="dang-ky-bo-cong-thuong">
+            </div>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <div class="ptthanhtoan">
+                <h3 class="header text-uppercase font-weight-bold">Phương thức thanh toán</h3>
+                <img src="tem/images/visa-payment.jpg" alt="visa-payment">
+                <img src="tem/images/master-card-payment.jpg" alt="master-card-payment">
+                <img src="tem/images/jcb-payment.jpg" alt="jcb-payment">
+                <img src="tem/images/atm-payment.jpg" alt="atm-payment">
+                <img src="tem/images/cod-payment.jpg" alt="cod-payment">
+                <img src="tem/images/payoo-payment.jpg" alt="payoo-payment">
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,13 +1,35 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="vi">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @include('web.head')
 </head>
+
 <body>
-    <h1>Day la trang chu khach hang</h1>
+<!-- code cho nut like share facebook  -->
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0"></script>
+
+
+
+<!-- header -->
+@include('web.header')
+<!-- khoi sach moi  -->
+
+@include('web.body')
+
+<!-- footer  -->
+<footer>
+    @include('web.footer')
+</footer>
+
+<!-- nut cuon len dau trang -->
+<div class="fixed-bottom">
+    <div class="btn btn-warning float-right rounded-circle nutcuonlen" id="backtotop" href="#"
+         style="background:#CF111A;"><i class="fa fa-chevron-up text-white"></i></div>
+</div>
+
 </body>
+
 </html>
